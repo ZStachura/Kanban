4f6db72e9f533a884d768679726f29e25c569099
ZStachura <zmstachura@gmail.com>
2022-01-23 16:08:46 +0100

    0.3 -> Change UL to Div

--- script.js
+++ script.js
@@ -2,12 +2,40 @@ window.onload=function(){
 document.getElementById("newTask").addEventListener("keyup", function(event) {
     if (event.code === 'Enter') {
     var text = document.getElementById("newTask").value;
-    var node=document.createElement("LI");
-    node.draggable = true;
+    var node=document.createElement("div");
     var newNode=document.createTextNode(text);
     node.appendChild(newNode);
+    node.setAttribute("class","task")
+    node.setAttribute("draggable","true")
+    node.setAttribute("ondragstart","onDragStart(event)")
     document.getElementById("toDoList").appendChild(node);
 }});
 
+function onDragStart(event) {
+    event
+      .dataTransfer
+      .setData('text/plain', event.target.id);
+  }
+
+  function onDragStart(event) {
+    event
+      .dataTransfer
+      .setData('text/plain', event.target.id);
+  }
+
+  function onDragOver(event) {
+    event.preventDefault();
+  }
+
+  function onDrop(event) {
+    const id = event
+      .dataTransfer
+      .getData('text');
+      const draggableElement = document.getElementById(id);
+      const dropzone = event.target;
+      event
+      .dataTransfer
+      .clearData();
+  }
 
 }
